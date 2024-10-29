0b288bbad7543520513a26587d7de698d7c47940
Daniluxa <shumkin.daniil@mail.ru>
2024-10-29 11:25:19 +0300

    Добавил работу по кнопке

--- PIO_projects/Blink_Test/src/main.c
+++ PIO_projects/Blink_Test/src/main.c
@@ -14,20 +14,32 @@
 #include "rom/gpio.h"
 
 #define GPIO_LED GPIO_NUM_2 
+#define GPIO_BTN GPIO_NUM_4 
+
+void btn_config(int bnt_num)
+{
+    gpio_pad_select_gpio(bnt_num); // режим ввода-вывода
+    gpio_set_direction(bnt_num, GPIO_MODE_INPUT); // вход
+    gpio_pullup_en(bnt_num); // резистор подключен к питанию
+    gpio_pulldown_dis(bnt_num); // резистор отключен от земли
+}
+
+void led_config(int led_num)
+{
+    gpio_pad_select_gpio(led_num); // режим ввода-вывода
+    gpio_set_direction(led_num, GPIO_MODE_OUTPUT); // активный выход   
+}
 
 void app_main(void)
 {
-    gpio_pad_select_gpio(GPIO_LED);
-    gpio_set_direction(GPIO_LED, GPIO_MODE_OUTPUT);
-    
+    btn_config(GPIO_BTN);
+    led_config(GPIO_LED);
+
     while(1)
     {
-        gpio_set_level(GPIO_LED, 0);
-        printf("Hello world!\n");
-        vTaskDelay(100);
+        gpio_set_level(GPIO_LED, gpio_get_level(GPIO_BTN));
+        printf("Состояние кнопки = %d", gpio_get_level(GPIO_BTN));
 
-        gpio_set_level(GPIO_LED, 1);
-        printf("Hello world!\n");
-        vTaskDelay(100);
+        vTaskDelay(250 / portTICK_PERIOD_MS); //задержка 0,25 сек
     }
 }
