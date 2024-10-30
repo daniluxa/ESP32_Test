bac684f757b1d66478f93a5227fd3d311723fa80
Daniluxa <shumkin.daniil@mail.ru>
2024-10-30 08:45:25 +0300

    Добавил прерывание для таймера

--- PIO_projects/Timers/src/main.c
+++ PIO_projects/Timers/src/main.c
@@ -7,39 +7,71 @@
 #include "driver/gpio.h"
 #include "rom/gpio.h"
 #include "driver/timer.h"
+#include "esp_timer.h"
+#include "driver/periph_ctrl.h"
 
 #define GPIO_LED GPIO_NUM_2 
 #define GPIO_BTN GPIO_NUM_4 
 
-void timer_callback(void *param)
+bool flag_timer = false;
+
+// Обработчик прерывания по нажатию кнопки
+static void IRAM_ATTR isrButtonPress(void* arg)
 {
-  static bool ON;
-  ON = !ON;
-  
-  gpio_set_level(GPIO_NUM_2, ON);
+
 }
 
-esp_err_t timer0_0_init()
+static void IRAM_ATTR timer_isr(void* arg) 
 {
+    gpio_set_level(GPIO_LED, 0);
+    flag_timer = true;
+}
+
+esp_err_t timer0_0_init(int timer_group, int timer_num)
+{
+    // конфигурация таймера
     timer_config_t myTimer;
-    myTimer.alarm_en = TIMER_ALARM_DIS;
-    myTimer.auto_reload = TIMER_AUTORELOAD_DIS;
+    myTimer.alarm_en = TIMER_ALARM_EN;
+    myTimer.auto_reload = TIMER_AUTORELOAD_EN;
     myTimer.clk_src = TIMER_SRC_CLK_DEFAULT;
     myTimer.counter_dir = TIMER_COUNT_UP ;
     myTimer.counter_en = TIMER_PAUSE;
     myTimer.divider = 80;
     myTimer.intr_type = TIMER_INTR_LEVEL;
+    esp_err_t err = timer_init(timer_group, timer_num, &myTimer);
 
-    esp_err_t err = timer_init(TIMER_GROUP_0, TIMER_0, &myTimer);
+    // настраиваем прерывание
+    timer_init(timer_group, timer_num, &myTimer);
+    timer_set_counter_value(timer_group, timer_num, 0);
+    timer_set_alarm_value(timer_group, timer_num, 2000000);
+    timer_enable_intr(timer_group, timer_num);
+    timer_isr_callback_add(timer_group, timer_num, timer_isr, NULL, 0);
+
     return err;
 }
 
 void btn_config(int bnt_num)
 {
+    esp_err_t err = gpio_install_isr_service(0);
+    if (err == ESP_ERR_INVALID_STATE) 
+    {
+        printf("ISR GPIO isr service already installed");
+    };
+
+    // Настраиваем вывод для кнопки
     gpio_pad_select_gpio(bnt_num); // режим ввода-вывода
     gpio_set_direction(bnt_num, GPIO_MODE_INPUT); // вход
     gpio_pullup_en(bnt_num); // резистор подключен к питанию
     gpio_pulldown_dis(bnt_num); // резистор отключен от земли
+
+    // Регистрируем обработчик прерывания на нажатие кнопки
+    gpio_isr_handler_add(bnt_num, isrButtonPress, NULL);
+    
+    // Устанавливаем тип события для генерации прерывания - по низкому уровню
+    gpio_set_intr_type(bnt_num, GPIO_INTR_NEGEDGE);
+    
+    // Разрешаем использование прерываний
+    gpio_intr_enable(bnt_num);
 }
 
 void led_config(int led_num)
@@ -51,7 +83,7 @@ void led_config(int led_num)
 void app_main() 
 {
     double timer0_0_value = 0;
-    timer0_0_init();
+    timer0_0_init(TIMER_GROUP_0, TIMER_0);
     btn_config(GPIO_BTN);
     led_config(GPIO_LED);
 
@@ -59,10 +91,12 @@ void app_main()
 
     while (1)
     {
-        if(gpio_get_level(GPIO_BTN) == 0)
+
+        if(flag_timer)
         {
-            timer_get_counter_time_sec(TIMER_GROUP_0, TIMER_0, &timer0_0_value);
-            printf("Timer value is %f\n", timer0_0_value);
-        } 
+            flag_timer = false;
+            printf("Timer interrupt\n");
+        }
+        vTaskDelay(1);
     }
 }
