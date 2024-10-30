17b4ef4026cfc7f5de605ab272b1cc05a9ad94ac
Daniluxa <shumkin.daniil@mail.ru>
2024-10-30 12:47:26 +0300

    Добавил отступы)))

--- PIO_projects/MyLibs/src/gpio.c
+++ PIO_projects/MyLibs/src/gpio.c
@@ -9,10 +9,10 @@ void gpio_init(int bnt_num, int gpio_mode, int pull_up, int pull_down, int isr_p
     // gpio configuration
     gpio_config_t MyGpio;
     MyGpio.pin_bit_mask = (1ULL << bnt_num);
-    MyGpio.mode = gpio_mode;
-    MyGpio.pull_up_en = pull_up;
+    MyGpio.mode         = gpio_mode;
+    MyGpio.pull_up_en   = pull_up;
     MyGpio.pull_down_en = pull_down;
-    MyGpio.intr_type = isr_posedge;
+    MyGpio.intr_type    = isr_posedge;
     
     gpio_config(&MyGpio); 
 
--- PIO_projects/MyLibs/src/tim.c
+++ PIO_projects/MyLibs/src/tim.c
@@ -6,13 +6,14 @@ esp_err_t timer0_0_init()
 {
     // timer confiration
     timer_config_t myTimer;
-    myTimer.alarm_en = TIMER_ALARM_EN;
+    myTimer.alarm_en    = TIMER_ALARM_EN;
     myTimer.auto_reload = TIMER_AUTORELOAD_EN;
-    myTimer.clk_src = TIMER_SRC_CLK_DEFAULT;
+    myTimer.clk_src     = TIMER_SRC_CLK_DEFAULT;
     myTimer.counter_dir = TIMER_COUNT_UP ;
-    myTimer.counter_en = TIMER_PAUSE;
-    myTimer.divider = 80; // 1MHz
-    myTimer.intr_type = TIMER_INTR_LEVEL;
+    myTimer.counter_en  = TIMER_PAUSE;
+    myTimer.divider     = 80; // 1MHz
+    myTimer.intr_type   = TIMER_INTR_LEVEL;
+
     esp_err_t err = timer_init(TIMER_GROUP_0, TIMER_0, &myTimer);
 
     // interrupt configuration
@@ -29,13 +30,14 @@ esp_err_t timer0_1_init()
 {
     // timer confiration
     timer_config_t myTimer;
-    myTimer.alarm_en = TIMER_ALARM_EN;
+    myTimer.alarm_en    = TIMER_ALARM_EN;
     myTimer.auto_reload = TIMER_AUTORELOAD_EN;
-    myTimer.clk_src = TIMER_SRC_CLK_DEFAULT;
+    myTimer.clk_src     = TIMER_SRC_CLK_DEFAULT;
     myTimer.counter_dir = TIMER_COUNT_UP ;
-    myTimer.counter_en = TIMER_PAUSE;
-    myTimer.divider = 80; // 1MHz
-    myTimer.intr_type = TIMER_INTR_LEVEL;
+    myTimer.counter_en  = TIMER_PAUSE;
+    myTimer.divider     = 80; // 1MHz
+    myTimer.intr_type   = TIMER_INTR_LEVEL;
+
     esp_err_t err = timer_init(TIMER_GROUP_0, TIMER_1, &myTimer);
 
     // interrupt configuration
@@ -52,13 +54,14 @@ esp_err_t timer1_0_init()
 {
     // timer confiration
     timer_config_t myTimer;
-    myTimer.alarm_en = TIMER_ALARM_EN;
+    myTimer.alarm_en    = TIMER_ALARM_EN;
     myTimer.auto_reload = TIMER_AUTORELOAD_EN;
-    myTimer.clk_src = TIMER_SRC_CLK_DEFAULT;
+    myTimer.clk_src     = TIMER_SRC_CLK_DEFAULT;
     myTimer.counter_dir = TIMER_COUNT_UP ;
-    myTimer.counter_en = TIMER_PAUSE;
-    myTimer.divider = 80; // 1MHz
-    myTimer.intr_type = TIMER_INTR_LEVEL;
+    myTimer.counter_en  = TIMER_PAUSE;
+    myTimer.divider     = 80; // 1MHz
+    myTimer.intr_type   = TIMER_INTR_LEVEL;
+
     esp_err_t err = timer_init(TIMER_GROUP_1, TIMER_0, &myTimer);
 
     // interrupt configuration
@@ -75,13 +78,14 @@ esp_err_t timer1_1_init()
 {
     // timer confiration
     timer_config_t myTimer;
-    myTimer.alarm_en = TIMER_ALARM_EN;
+    myTimer.alarm_en    = TIMER_ALARM_EN;
     myTimer.auto_reload = TIMER_AUTORELOAD_EN;
-    myTimer.clk_src = TIMER_SRC_CLK_DEFAULT;
+    myTimer.clk_src     = TIMER_SRC_CLK_DEFAULT;
     myTimer.counter_dir = TIMER_COUNT_UP ;
-    myTimer.counter_en = TIMER_PAUSE;
-    myTimer.divider = 80; // 1MHz
-    myTimer.intr_type = TIMER_INTR_LEVEL;
+    myTimer.counter_en  = TIMER_PAUSE;
+    myTimer.divider     = 80; // 1MHz
+    myTimer.intr_type   = TIMER_INTR_LEVEL;
+
     esp_err_t err = timer_init(TIMER_GROUP_1, TIMER_1, &myTimer);
 
     // interrupt configuration
