8854b2d8fdb31b9fbc28aa14d749d9f5f76d48c7
Daniluxa <shumkin.daniil@mail.ru>
2024-10-30 10:29:41 +0300

    Добавил библиотеку для gpio

--- PIO_projects/Blink_Test/src/main.c
+++ PIO_projects/Blink_Test/src/main.c
@@ -3,15 +3,8 @@
  *
  * SPDX-License-Identifier: CC0-1.0
  */
-
-#include <stdio.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "driver/gpio.h"
-#include "rom/gpio.h"
+#include "main.h"
+#include "gpio.h"
 
 #define GPIO_LED GPIO_NUM_2 
 #define GPIO_BTN GPIO_NUM_4 
@@ -26,37 +19,11 @@ static void IRAM_ATTR isrButtonPress(void* arg)
         btn_flag = false;
 }
 
-void btn_config(int bnt_num)
-{
-    // Устанавливаем сервис GPIO ISR service
-    esp_err_t err = gpio_install_isr_service(0);
-    
-    // Настраиваем вывод для кнопки
-    gpio_pad_select_gpio(bnt_num); // режим ввода-вывода
-    gpio_set_direction(bnt_num, GPIO_MODE_INPUT); // вход
-    gpio_pullup_en(bnt_num); // резистор подключен к питанию
-    gpio_pulldown_dis(bnt_num); // резистор отключен от земли
-
-    // Регистрируем обработчик прерывания на нажатие кнопки
-    gpio_isr_handler_add(bnt_num, isrButtonPress, NULL);
-    
-    // Устанавливаем тип события для генерации прерывания - по низкому уровню
-    gpio_set_intr_type(bnt_num, GPIO_INTR_ANYEDGE);
-    
-    // Разрешаем использование прерываний
-    gpio_intr_enable(bnt_num);
-}
-
-void led_config(int led_num)
-{
-    gpio_pad_select_gpio(led_num); // режим ввода-вывода
-    gpio_set_direction(led_num, GPIO_MODE_OUTPUT); // активный выход   
-}
-
 void app_main(void)
 {
-    btn_config(GPIO_BTN);
-    led_config(GPIO_LED);
+    gpio_init(GPIO_BTN, GPIO_MODE_INPUT, GPIO_PULLUP_ENABLE, GPIO_PULLDOWN_DISABLE, GPIO_INTR_ANYEDGE, &isrButtonPress);
+    gpio_init(GPIO_LED, GPIO_MODE_OUTPUT, GPIO_PULLUP_DISABLE, GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE, NULL);
+
     bool prev_flag = btn_flag;
     while(1)
     {
@@ -65,5 +32,6 @@ void app_main(void)
             printf("%d\n", btn_flag);
             prev_flag = btn_flag;
         }
+        vTaskDelay(1);
     }
 }
